1f642d382d3988abf0d5a92f36759a4c822c3aba
Ayush Kumar <ks.ayush2618@gmail.com>
2025-09-20 22:18:12 +0530

    part 10

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,33 @@
 
 // export default nextConfig;
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: [
+//       'cdn.shopify.com',
+//       'images.unsplash.com',
+//       'anotherglobaleater.wordpress.com',
+//       'm.media-amazon.com'
+//     ],
+//   },
+// }
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: [
+      // Your existing domains
       'cdn.shopify.com',
       'images.unsplash.com',
       'anotherglobaleater.wordpress.com',
-      'm.media-amazon.com'
+      'm.media-amazon.com',
+      
+      'images.slurrp.com',
+      'upload.wikimedia.org',
+      'www.mangodatabase.com',
     ],
   },
 }
